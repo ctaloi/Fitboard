1ecbde1f3edfea08e4b8150cdb5df27d0444260a
Chris <ctaloi@gmail.com>
2013-06-12 22:12:54 -0400

    Pulled running slug off Heroku to fix, not sure what I did.  Need to be more careful.. Was related to sessions, then I got lost in gitland

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+SQLALCHEMY_DATABASE_URI = 'postgres://qwcucsqoeopfam:-3CwkaK4_nkCmFqyly0Rkd2Po8@ec2-23-21-129-229.compute-1.amazonaws.com:5432/d9kajj9pmpem7c'
+SECRET_KEY = '6Nue]A=U+3t%4@'
+DEBUG = True
+STATHAT_EZ_KEY = 'ctaloi@gmail.com'
+STATHAT_USE_GEVENT = False
+DEBUG_TB_INTERCEPT_REDIRECTS = False
