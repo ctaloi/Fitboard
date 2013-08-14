464f45e53fdab86ea5a56145dec7254f2b940e04
Chris Aloi <ctaloi@gmail.com>
2013-08-14 15:59:38 -0400

    PEP8

--- app.py
+++ app.py
@@ -34,8 +34,10 @@ if MY_PLATFORM == 'LOCAL':
     import logging
     from logging import Formatter
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('fitboard.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler = RotatingFileHandler(
+        'fitboard.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(
+        logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
